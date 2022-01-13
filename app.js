5fd31f58fdbc3392d89d2be1d86ed1c091341e04
Donia Shaker <donia.shaker11@gmail.com>
2022-01-13 18:55:54 +0300

    Add Action To Image In Dark Mode

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+//function of Dark-Light Mode
 function changeMode() {
   var checkBox = document.getElementById("switch");
+  var action = document.getElementById("action"); //add action ti image
   if (checkBox.checked) {
     document.documentElement.style.setProperty("--background-color", "#111");
     document.documentElement.style.setProperty("--main-color", "#fff");
+    action.style.opacity = "65%"; //add action to image
   } else {
     document.documentElement.style.setProperty("--background-color", "#fff");
     document.documentElement.style.setProperty("--main-color", "#111");
+    action.style.opacity = "1"; //add action to image
   }
 }
