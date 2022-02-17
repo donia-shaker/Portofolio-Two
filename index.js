4abe3c1e5da6e261419312112782984775f4b9f3
Donia Shaker <donia.shaker11@gmail.com>
2022-02-17 15:02:05 +0300

    add public folder

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const exp = require("constants");
+const path = require("path");
 const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static("views/css"));
-app.use(express.static("views"));
-app.use(express.static("views/images"));
+app.use(express.static(path.resolve(__dirname, "public")));
+
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.listen("3000");
+app.listen("3080");
